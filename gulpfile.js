ab995118a713cc8b764cd089579a92aca19108f1
zhuhuijun <zhuhuijun@mofangge.com>
2015-11-03 19:28:20 +0800

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,25 @@ gulp.task('hello', function () {
 gulp.task('word', function () {
     console.log('word');
 });
-
+/**
+ * copy file
+ */
+gulp.task('copyhtml', function () {
+    return gulp.src('./app/index.html').pipe(gulp.dest('dist'));
+});
+gulp.task('copypng', function () {
+    return gulp.src('./app/images/*.png').pipe(gulp.dest('dist/images'));
+});
+gulp.task('copypng1', function () {
+    return gulp.src('./app/images/*.{png,jpg}').pipe(gulp.dest('dist/images'));
+});
+gulp.task('copypng2', function () {
+    return gulp.src('./app/images/**').pipe(gulp.dest('dist/images'));
+});
+gulp.task('copypng2', function () {
+    //return gulp.src(['./app/images*//**','!./app/js*//**','./app/css*//**']).pipe(gulp.dest('dist/images'));//---！是不要的意思
+    return gulp.src(['./app/images/**','./app/js/**','./app/css/**']).pipe(gulp.dest('dist/images'));
+});
 /*
  gulp.task('default', function () {
  console.log('default');
@@ -13,6 +31,13 @@ gulp.task('word', function () {
 /***
  * 定义组合任务
  */
-gulp.task('default', ['hello', 'word'], function () {
+gulp.task('watch',function(){
+    gulp.watch('./app/index.html',['copyhtml']);
+});
+
+gulp.task('watch',function(){
+    gulp.watch('./app/index.html',['copyhtml']);
+});
+gulp.task('default', ['watch'], function () {
     console.log('done');
 });
