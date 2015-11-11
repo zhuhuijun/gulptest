e9bb200be6d4b53795e7a511ec1926792b703e1c
zhuhuijun <zhuhuijun@mofangge.com>
2015-11-11 13:04:41 +0800

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,28 @@
 var gulp = require('gulp');
+var less = require('gulp-less');
+var scss = require('gulp-scss');
+var connect = require('gulp-connect');//实时刷新
+var concat = require('gulp-concat');//连接
+var uglify=require('gulp-uglify');//压缩
+var rename=require('gulp-rename');//重命名
 gulp.task('hello', function () {
     console.log('hello');
 });
 gulp.task('word', function () {
     console.log('word');
 });
+gulp.task('server', function () {
+    connect.server({
+        root: 'dist',//根目录
+        port: 4000,
+        livereload: true//实时刷新
+    });
+});
 /**
  * copy file
  */
 gulp.task('copyhtml', function () {
-    return gulp.src('./app/index.html').pipe(gulp.dest('dist'));
+    return gulp.src('./app/index.html').pipe(gulp.dest('dist')).pipe(connect.reload());
 });
 gulp.task('copypng', function () {
     return gulp.src('./app/images/*.png').pipe(gulp.dest('dist/images'));
@@ -22,7 +35,7 @@ gulp.task('copypng2', function () {
 });
 gulp.task('copypng2', function () {
     //return gulp.src(['./app/images*//**','!./app/js*//**','./app/css*//**']).pipe(gulp.dest('dist/images'));//---！是不要的意思
-    return gulp.src(['./app/images/**','./app/js/**','./app/css/**']).pipe(gulp.dest('dist/images'));
+    return gulp.src(['./app/images/**', './app/js/**', './app/css/**']).pipe(gulp.dest('dist/images'));
 });
 /*
  gulp.task('default', function () {
@@ -31,13 +44,22 @@ gulp.task('copypng2', function () {
 /***
  * 定义组合任务
  */
-gulp.task('watch',function(){
-    gulp.watch('./app/index.html',['copyhtml']);
+gulp.task('watch', function () {
+    gulp.watch('./app/index.html', ['copyhtml']);
 });
 
-gulp.task('watch',function(){
-    gulp.watch('./app/index.html',['copyhtml']);
+gulp.task('scss', function () {
+    return gulp.src('app/css/*.sass').pipe(scss()).pipe(gulp.dest('dist/css'));
 });
-gulp.task('default', ['watch'], function () {
+gulp.task('less', function () {
+    return gulp.src('app/css/*.less').pipe(less()).pipe(gulp.dest('dist/css'));
+});
+gulp.task('concatjs1', function () {
+    return gulp.src(['app/js/*.js']).pipe(concat('join.js')).pipe(gulp.dest('dist/js'));
+});
+gulp.task('concatjs', function () {
+    return gulp.src(['app/js/*.js']).pipe(concat('join.js')).pipe(uglify()).pipe(rename('join.min.js')).pipe(gulp.dest('dist/js'));
+});
+gulp.task('default', ['server', 'less', 'scss', 'watch'], function () {
     console.log('done');
 });
